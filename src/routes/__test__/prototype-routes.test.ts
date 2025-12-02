13db2fc0595f249951362ac879632b12700eb8ca
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2025-12-02 16:28:53 +0000

    Create additional tests for renderPrototypePage

--- src/routes/__test__/prototype-routes.test.ts
+++ src/routes/__test__/prototype-routes.test.ts
@@ -787,9 +787,8 @@ describe('handleLivePrototypePasswordSubmission', () => {
         res: any
     ) => Promise<void>;
     beforeEach(async () => {
-        ({ handleLivePrototypePasswordSubmission } = await import(
-            '../prototype-routes'
-        ));
+        ({ handleLivePrototypePasswordSubmission } =
+            await import('../prototype-routes'));
     });
 
     it('should render 404 if prototype not found', async () => {
@@ -896,9 +895,8 @@ describe('handlePrototypeSubmitQuestion', () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     let handlePrototypeSubmitQuestion: (req: any, res: any) => void;
     beforeEach(async () => {
-        ({ handlePrototypeSubmitQuestion } = await import(
-            '../prototype-routes'
-        ));
+        ({ handlePrototypeSubmitQuestion } =
+            await import('../prototype-routes'));
     });
 
     it.each([{}, { liveData: {} }])(
@@ -1140,11 +1138,16 @@ describe('renderPrototypePage', () => {
         expect(response._getData()).toBe('rendered-html');
     });
 
-    it('should render the confirmation page and reset liveData', () => {
+    it('should render the confirmation page and reset the session data', () => {
         const request = httpMocks.createRequest({
             ...defaultRequest,
             params: { id: prototypeData1.id, page: 'confirmation' },
-            session: { liveData: { [prototypeData1.id]: { some: 'data' } } },
+            session: {
+                liveData: { [prototypeData1.id]: { some: 'data' } },
+                livePrototypeHistory: {
+                    [prototypeData1.id]: ['/url1', '/url2'],
+                },
+            },
         });
         const response = httpMocks.createResponse();
 
@@ -1153,6 +1156,9 @@ describe('renderPrototypePage', () => {
         expect(generateConfirmationPageMock).toHaveBeenCalled();
         expect(generateBasePageMock).toHaveBeenCalled();
         expect(request.session.liveData?.[prototypeData1.id]).toStrictEqual({});
+        expect(
+            request.session.livePrototypeHistory?.[prototypeData1.id]
+        ).toStrictEqual([]);
         expect(nunjucksRenderStringMock).toHaveBeenCalledWith(
             expect.stringContaining('confirmation-content'),
             expect.any(Object)
@@ -1175,6 +1181,87 @@ describe('renderPrototypePage', () => {
             expect(response._getRenderView()).toBe('page-not-found.njk');
         }
     );
+
+    it('removes the last URL from the history if the user navigated back', () => {
+        const request = httpMocks.createRequest({
+            ...defaultRequest,
+            params: { id: prototypeData1.id, page: 'question-1' },
+            query: { back: 'true' },
+            session: {
+                livePrototypeHistory: {
+                    [prototypeData1.id]: ['/url1', '/url2'],
+                },
+            },
+        });
+        const response = httpMocks.createResponse();
+
+        renderPrototypePage(request, response);
+
+        expect(
+            request.session.livePrototypeHistory?.[prototypeData1.id]
+        ).toEqual(['/url1']);
+    });
+
+    it('adds the URL to the history', () => {
+        const request = httpMocks.createRequest({
+            ...defaultRequest,
+            params: { id: prototypeData1.id, page: 'question-1' },
+            session: {
+                livePrototypeHistory: {
+                    [prototypeData1.id]: ['/url1'],
+                },
+            },
+            url: '/url2',
+        });
+        const response = httpMocks.createResponse();
+
+        renderPrototypePage(request, response);
+
+        expect(
+            request.session.livePrototypeHistory?.[prototypeData1.id]
+        ).toEqual(['/url1', '/url2']);
+    });
+
+    it('does not add duplicate URLs to the history', () => {
+        const request = httpMocks.createRequest({
+            ...defaultRequest,
+            params: { id: prototypeData1.id, page: 'question-1' },
+            session: {
+                livePrototypeHistory: {
+                    [prototypeData1.id]: ['/url1', '/url2'],
+                },
+            },
+            url: '/url2',
+        });
+        const response = httpMocks.createResponse();
+
+        renderPrototypePage(request, response);
+
+        expect(
+            request.session.livePrototypeHistory?.[prototypeData1.id]
+        ).toEqual(['/url1', '/url2']);
+    });
+
+    it('sets the back link', () => {
+        const request = httpMocks.createRequest({
+            ...defaultRequest,
+            params: { id: prototypeData1.id, page: 'question-1' },
+            session: {
+                livePrototypeHistory: {
+                    [prototypeData1.id]: ['/url1', '/url2'],
+                },
+            },
+            url: '/url3',
+        });
+        const response = httpMocks.createResponse();
+
+        renderPrototypePage(request, response);
+
+        expect(nunjucksRenderStringMock).toHaveBeenCalledWith(
+            expect.anything(),
+            expect.objectContaining({ backLinkHref: '/url2?back=true' })
+        );
+    });
 });
 
 describe('renderResultsPage', () => {
